8620e462ce6086d27d177423a1460224dcb1cd40
greedy17 <61763082+greedy17@users.noreply.github.com>
2020-11-21 13:51:23 -0500

    added object validation for GET and POST endpoints

---
+++ validators/custom-validatiolns.js
@@ -0,0 +1,32 @@
+const { isNullOrUndefined, isString, isNumber } = require("util");
+
+function isNotEmptyAndIsString(value) {
+ return !isNullOrUndefined(value) && isString(value);
+}
+
+function isNotEmptyAndIsNumber(value) {
+ return !isNullOrUndefined(value) && isNumber(value);
+}
+
+exports.body = (req, res, next) => {
+ let method = req.method;
+ if (method == "POST" || method == "PUT") {
+ let data = req.body;
+ if (
+ (method == "POST"
+ ? isNullOrUndefined(data.id)
+ : isNotEmptyAndIsNumber(data.id)) &&
+ isNotEmptyAndIsString(data.title) &&
+ isNotEmptyAndIsString(data.director) &&
+ isNotEmptyAndIsString(data.genre)
+ ) {
+ next();
+ } else {
+ res.status(400).send({
+ error: { status: 400, description: "Invalid object format." },
+ });
+ }
+ } else {
+ next();
+ }
+};
