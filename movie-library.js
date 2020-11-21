f73a5f0f9f69d31849b4ad58b514a541c88eea86
greedy17 <61763082+greedy17@users.noreply.github.com>
2020-11-21 15:29:02 -0500

    added html file to display movie app. Added basic styling(will be changed when functionality complete), added fully functional search bar

---
+++ movie-library.js
@@ -0,0 +1,36 @@
+function getMovies(){
+  $(function(){
+    $.ajax({
+      type: 'GET',
+      url: "http://localhost:3000/api/movies",
+      dataType: 'json',
+      success: function(){
+        $('.library').html('');
+      }
+    })
+    .then(function(data){
+      $.each(data, function(index, value){
+        $('.library').append(
+          '<tr>' +
+          '<div>' + '<td>' + "Movie: " + value.title + '</td>' + '</div>' +
+          '<div>' + '<td>' + "Director: " + value.director + '</td>' + '</div>' +
+          '<div>' + '<td>' + "Genre: " + value.genre + '</td>' + '</div>' +
+          '</tr>'
+        );
+      });
+    });
+  });
+}
+
+getMovies();
+
+function searchMovies(){
+  $(function(){
+    $("#input").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myTable tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
+}
