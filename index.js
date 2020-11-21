c103e3e512fa3e0884f82ab075ba83484a45bacc
greedy17 <61763082+greedy17@users.noreply.github.com>
2020-11-21 13:44:32 -0500

    added CRUD endpoints

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const repoContext = require("./repository/repository-wrapper.js");
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
-  console.log('server started. listening on port 3000.')
+  console.log('Server started. listening on port 3000.')
 });
+
+app.get("/api/movies", (req, res) => {
+  let movies = repoContext.movies.findAllMovies();
+  res.send(movies);
+})
+
+app.get('/api/movies/:id', (req, res) => {
+  let id = req.params.id;
+  let movie = repoContext.movies.findMovieById(id);
+  res.send(movie)
+});
+
+app.post("/api/movies", (req, res) => {
+  let newMovie = req.body;
+  let addedMovie = repoContext.movies.createMovie(newMovie);
+  res.send(addedMovie);
+});
+
+app.put("/api/movies", (req, res) => {
+  let movieToUpdate = req.body;
+  let updatedMovie = repoContext.movies.updateMovie(movieToUpdate);
+  res.send(updatedMovie);
+});
+
+app.delete("/api/movies/:id", (req, res) => {
+  let id = req.params.id;
+  let updateDataSet = repoContext.movies.deleteMovie(id);
+  res.send(updateDataSet);
+})
