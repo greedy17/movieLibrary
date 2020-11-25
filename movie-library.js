1c816d553406e3838f040be746dbbec8e46696b3
Robert J Magaña <robjmagana@gmail.com>
2020-11-24 16:33:51 -0800

    Edited movie library api to include instructor comments
    Tony's edits

--- movie-library.js
+++ movie-library.js
@@ -15,16 +15,14 @@ function getMovies(){
           '<div>' + '<td>' + "Movie: " + value.title + '</td>' + '</div>' +
           '<div>' + '<td>' + "Director: " + value.director + '</td>' + '</div>' +
           '<div>' + '<td>' + "Genre: " + value.genre + '</td>' + '</div>' +
-          '<div>' + '<td>' + '<button id="update-movie">Update</button>' + '</td>' + '</div>' +
+          '<div>' + '<td>' + '<button>Update</button>' + '</td>' + '</div>' +
           '</tr>'
         );
       });
     });
   });
 }
-
 getMovies();
-
 function searchMovies(){
   $(function(){
     $("#input").on("keyup", function() {
@@ -35,25 +33,25 @@ function searchMovies(){
     });
   });
 }
-
-function addMovie(){
-$("#post").click(function(){
-  $.post("http://localhost:3000/api/movies", 
-    {
-      title: $("#1").val(),
-      director: $("#2").val(),
-      genre: $("#3").val()
-    }, "json")
-});
-}
-
-function updateMovie() {
+function addMovie(){ //tony says this function might not work right by now it is declared
+// $("#post").click(function(){ //making a listener (could comment this line out and make a valid api) (everytime addmovie is called it's making a listener)
+//line above is a working method now. listener may not be working the way it should be (onsubmit)
+  let title = $("#1").val();
+  let director = $("#2").val();
+  let genre = $("#3").val();
+  let data = {
+    title: title, 
+    director: director, 
+    genre: genre,
+  }
   $.ajax({
-    url: 'http://localhost:3000/api/movies',
-    type: 'PUT',
-    type: "json",
-    success: function(result) {
-        
+    url:"http://localhost:3000/api/movies", 
+    dataType: "json",
+    data: data,
+    type: "post",
+    success: function(data, textStatus, jQxhr){
+      console.log(data);
+      //call a success function
     }
 });
 }
